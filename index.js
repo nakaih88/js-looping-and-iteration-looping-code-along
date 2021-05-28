b6f13a3268e75acdf17ea5cf4e989207a596348a
Nakai <nakai.hamlin18@gmail.com>
2021-05-28 02:21:42 -0400

    commit tree 5242f79b58957002bd10c84e69019db3d716ce21

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names, event) {
+    let thankCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankCards
+}
